5e0b6df963fbe72da0a7246ebbe79a5fa0248cce
othmane triki <trikiotmane@gmail.com>
2025-12-12 14:37:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Notes-App/',   
+  base: '/Notes-app/',   
   plugins: [tailwindcss(), react()]
 })
